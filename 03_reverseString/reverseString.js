a950156adc75858a32f146e63ee5a83f405aa676
Carlo Pamenting <carlo.pamenting14@gmail.com>
2023-03-07 00:24:15 +0800

    Finished third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
-const reverseString = function() {
-
-};
-
-// Do not edit below this line
+const reverseString = function(random) {
+    return random.split("").reverse().join("");
+}
+//Do not edit below this line
 module.exports = reverseString;
