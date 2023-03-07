05e7aafc291779bbd2195c4033b067e2d1c81e18
Carlo Pamenting <carlo.pamenting14@gmail.com>
2023-03-08 00:24:02 +0800

    Finished fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(Number.isInteger(num1) && Number.isInteger(num2)){
+        if(num1 < num2 && num1 > 0 && num2 > 0){
+            for(i=num1; i <= num2; i++) {
+               sum += i;
+            }
+            return sum;
+        }
+        if(num1 > num2 && num1 > 0 && num2 > 0){
+            for(i=num1; i >= num2; i--) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+   
+    return "ERROR";
+  
 };
 
 // Do not edit below this line
